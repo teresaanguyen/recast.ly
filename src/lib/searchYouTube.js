1d0018c24b8dc5b5e33be48eb43b3c4bd4bc7d2c
Teresa Nguyen <88606876+teresaanguyen@users.noreply.github.com>
2021-10-05 14:15:42 -0700

    implemented ajax request

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -6,6 +6,19 @@ $.ajaxPrefilter(function (settings, _, jqXHR) {
 
 var searchYouTube = (query, callback) => {
   // TODO
+  $.ajax({
+    url: 'https://app-hrsei-api.herokuapp.com/api/recastly/videos',
+    type: 'GET',
+    data: {
+      q: query},
+    dataType: 'json',
+    success: function(data) {
+      callback(data);
+    },
+    error: function (error) {
+      console.error('chatterbox: Failed to fetch messages', error);
+    }
+  });
 };
 
 export default searchYouTube;
